d9dbf62713542f581124866df33f7304ea27656a
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-18 16:18:02 +0530

    added mobile toggle menu bar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import logo from '../../assets/images/logo.png'
 import {NavLink, Link} from 'react-router-dom'
 import userImg from '../../assets/images/avatar-icon.png'
@@ -27,8 +27,33 @@ const navLinks = [
 ]
 
 const Header = () => {
+
+  const headeRef = useRef(null);
+  const menuRef = useRef(null);
+
+
+  const handleStickyHeader = () => {
+
+    window.addEventListener('scroll', () => {
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop >80 ){
+        headeRef.current.classList.add('sticky__header')
+      } else {
+        headeRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    handleStickyHeader()
+
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  },[])
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
+
   return (
-    <header className='header flex items-center'>
+    <header className='header flex items-center' ref={headeRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* logo */}
@@ -39,7 +64,7 @@ const Header = () => {
 
         {/* menu section */} 
 
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
               {
                 navLinks.map((link,index)=>(
@@ -56,7 +81,7 @@ const Header = () => {
           {/* right nav section */}
 
           <div className="flex items-center gap-4">
-            <div>
+            <div className='hidden'>
               <Link to='/'>
                 <figure className='w-[35px] h-35px] rounded-full cursor-pointer'>
 
@@ -70,7 +95,7 @@ const Header = () => {
                 Login</button>
             </Link>
 
-            <span className='md:hidden'>
+            <span className='md:hidden' onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer'/>
             </span>
           </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <>
+      <section className='hero__section pt-[60px] 2xl:h-[800px] '>
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between ">
+            {/* hero content section */}
+            <div>
+              <div className="lg:w-[570px]">
+              <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]'>
+                  We help patients live a healthy, longer life.
+                </h1>
+                <p className='text__para'>Medicine, like most other fields, has undergone a rapid transformation, due to advancements in technology.
+                 The usage of mobile medical applications has increased worldwide.
+                 </p>
+
+                 <button className="btn">Request an Appointment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
