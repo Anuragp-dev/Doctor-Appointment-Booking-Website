30cbb3728018b686ecc2e407bcf1c7c2e71470c7
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-19 16:59:21 +0530

    added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -172,13 +172,27 @@ const Home = () => {
         </div>
       </section>
 
-{/* about section starts */}
+      {/* about section starts */}
+      <About />
+      {/* about sction ends */}
 
-<section>
-  <About/>
-</section>
 
-{/* about sction ends */}
+        {/* about sction starts */}
+
+          <section>
+            <div className="container">
+              <div className="xl:w[470px] mx-auto">
+                <h2 className='heading text-center'>Our medical services</h2>
+                <p className='text__para text-center'>World -class care for everyone. Our health system offers unmatched,
+              expert health care</p>
+              </div>
+            </div>
+          </section>
+          {/* about sction end */}
+
+
+
+
 
     </>
 
