2e014ad543ce15d6973b9e4e331938cabe622990
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-20 17:43:22 +0530

    added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,14 @@ import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from '../assets/images/avatar-icon.png';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/doctors/DoctorList';
+import FaqList from '../components/Faq/faqList';
+import faqImg from '../assets/images/faq-img.png';
+import Testimonial from '../components/Testimonial/Testimonial';
+
+
+
+
+
 const Home = () => {
   return (
     <>
@@ -272,7 +280,55 @@ const Home = () => {
   {/* our great doctors end */}
 
 
+{/* Faq section */}
 
+
+
+<section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className='w-1/2 hidden md:block'>
+              <img src={faqImg} alt="faq" />
+            </div>
+
+            <div className='w-full md:w-1/2'>
+              <h2 className='heading'>Most questions by our beloved patients</h2>
+
+              <FaqList/>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+
+
+
+      {/* Faq section end */}
+
+
+
+
+      {/* testimonial */}
+
+
+      <section>
+        <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+            <h2 className='heading text-center'>What our patients say</h2>
+            <p className="text__para text-center">World -class care for everyone. Our health system offers unmatched,
+              expert health care.
+            </p>
+          </div>
+
+    <Testimonial/>
+
+        </div>
+      </section>
+
+
+
+      {/* testimonial end */}
 
 
     </>
