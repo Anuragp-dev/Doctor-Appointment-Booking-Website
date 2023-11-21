1863d94f0404a9516312c8bd6683986a94f20067
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-21 12:12:46 +0530

    added

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
     <section>
       <div className="container">
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] '>
         {
             services.map((item,index) => ( 
                 <ServiceCard item={item} index={index} key={index}/>
