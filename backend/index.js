ed6fe12996e3c94ea6f2c738bc9321938040cbe8
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-22 13:36:10 +0530

    Backend started

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+
+
+dotenv.config()
+const app =  express()
+
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin:true
+}
+
+app.get('/', (req, res) => {
+    res.send('api is working')
+})
+
+//database connection
+
+mongoose.set('strictQuery', false);
+
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("mongo Database connected");
+    }catch(err) {
+            console.log("mongo Database falied");
+    }
+}
+
+
+//middleware
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+
+app.listen(port, () => {
+    connectDb();
+    console.log("server is running on port" + port);
+})
