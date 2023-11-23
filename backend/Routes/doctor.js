bf4e194a531acea227f4466e9d46457c7c228fc7
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-23 17:08:54 +0530

    update doctor routes

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import { deleteDoctor, getAllDoctor, getSingleDoctor, updateDoctor } from '../Controller/doctorController.js';
 import { authenticate, restrict } from '../auth/verifyToken.js';
+import reviewRouter from './review.js';
+
 
 
 
 const router = express.Router();
 
 
+
+//nested route
+router.use('/:doctorId/reviews', reviewRouter);
+
+
 router.get('/:id', getSingleDoctor);
 router.get('/', getAllDoctor);
-router.put('/:id', authenticate, restrict(['doctor']), updateDoctor);
-router.delete('/:id', authenticate, restrict(['doctor']), deleteDoctor);
+router.put('/:id',authenticate, restrict(['doctor']), updateDoctor);
+router.delete('/:id',authenticate, restrict(['doctor']), deleteDoctor);
 
 
 
