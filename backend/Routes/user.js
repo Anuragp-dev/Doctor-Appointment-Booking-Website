1ac29104e83b3eeb86f4924a2dec281b70b97c0a
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-25 15:11:22 +0530

    added

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,6 +6,7 @@ import { authenticate, restrict } from '../auth/verifyToken.js';
 
 const router = express.Router();
 
+//all routes
 
 router.get('/:id', authenticate, restrict(['patient']), getSingleUser);
 router.get('/', authenticate, restrict(['admin']), getAllUser);
