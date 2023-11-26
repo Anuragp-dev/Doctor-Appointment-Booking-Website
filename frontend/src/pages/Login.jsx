9e59cfb1e1f1ec8479aa5e67f43ab4c16a89a8ba
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-26 14:51:56 +0530

    login added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../config';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { authContext } from '../context/AuthContext.jsx';
+import HashLoader from 'react-spinners/HashLoader';
+
+
 
 const Login = () => {
+
+
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { dispatch } = useContext(authContext);
+
 
   const [formData, setFormData] = useState({
     email:'',
@@ -13,12 +28,57 @@ const Login = () => {
   }
 
 
+  const submitHandler = async event => {
+    // console.log(formData);
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method:'post',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+
+        body: JSON.stringify(formData)
+      });
+
+      const result = await res.json();
+
+      if (!res.ok) {
+        throw new Error(result.message)
+      }
+
+      dispatch({
+        type:'LOGIN_SUCCESS',
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role
+        }
+      });
+
+      console.log(result, "login data");
+
+      setLoading(false)
+      toast.success(result.message)
+      navigate('/home')
+
+    } catch (error) {
+      toast.error(error.message)
+      setLoading(false)
+    }
+  }
+
+
+
+
   return (
     <section className='px-5 lg:px-0'>
       <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
         <h3 className='text-textColor text-[22px] leading-9 font-bold mb-10'>
           Hello! <span className='text-primaryColor'>Welcome</span> Back</h3>
-        <form className='py-4 md:py-0' action="">
+        <form className='py-4 md:py-0' onSubmit={submitHandler} action="">
 
           <div className='mb-5'>
             <input type="email" placeholder='Enter your email' name='email'
@@ -41,7 +101,7 @@ const Login = () => {
 
           <div className='mt-7'>
             <button className='w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3'
-             type='submit'>Login</button>
+             type='submit'>{ loading ? <HashLoader size={25} color='#fff'/> : 'Login'}</button>
           </div>
 
           <p className='mt-5 text-textColor text-center'>Don&apos;t have an account?
