f14e08ec0e94ab97011c5f707e5b46790b0f75b1
Anurag p <107052990+Anuragp212@users.noreply.github.com>
2023-11-26 15:31:03 +0530

    pending payment gateway and dashboards updating soon

---
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { authContext } from "../context/AuthContext";
+import {Navigate} from 'react-router-dom';
+
+
+const ProtectedRoutes = ({ children, allowedRoles }) => {
+
+    const { token, role } = useContext(authContext)
+
+    const isAllowed = allowedRoles.includes(role)
+
+    const accessibleRoute = token && isAllowed ? children : <Navigate to='/login' replace={true} />
+
+    
+  return (
+   accessibleRoute
+  );
+}
+
+export default ProtectedRoutes
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from '../pages/Home'
-import Services from '../pages/Services'
-import Contact from '../pages/Contact'
-import Login from '../pages/Login'
-import SignUp from '../pages/SignUp'
-import Doctors from '../pages/Doctors/Doctors'
-import DoctorsDetails from '../pages/Doctors/DoctorDetails'
+import React from 'react';
+import Home from '../pages/Home';
+import Service from '../pages/Services';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Contact from '../pages/Contact';
+import Doctors from '../pages/Doctors/Doctors';
+import DoctorDetails from '../pages/Doctors/DoctorDetails';
+
+import {Routes, Route} from 'react-router-dom';
+import MyAccount from '../Dashboard/user-account/MyAccount';
+import Dashboard from '../Dashboard/doctor-account/Dashboard';
+import ProtectedRoutes from './ProtectedRoutes';
+
+
 
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/home' element={<Home/>}/>
-      <Route path='/service' element={<Services/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<SignUp/>}/>
-      <Route path='/doctors' element={<Doctors/>}/>
-      <Route path='/doctors/:id' element={<DoctorsDetails/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:id' element={<DoctorDetails/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Signup/>}/>
+        <Route path='/service' element={<Service/>}/>
+        <Route path='/users/profile/me' element={<ProtectedRoutes allowedRoles={['patient']}> <MyAccount/> </ProtectedRoutes>}/>
+        <Route path='/doctors/profile/me' element={<ProtectedRoutes allowedRoles={['doctor']}> <Dashboard/> </ProtectedRoutes>}/>
     </Routes>
   )
 }
 
-export default Router
+export default Router;
