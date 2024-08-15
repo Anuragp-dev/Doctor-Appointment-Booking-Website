b771cfd8285987e4665ed6b467dafe65025df287
ANURAG P <anuragp212@gmail.com>
2024-08-15 12:55:14 +0530

    Update useFetchData.jsx

--- frontend/src/hooks/useFetchData.jsx
+++ frontend/src/hooks/useFetchData.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { token } from "../config";
 
 
-// usefetch data
+// usefetch dat
 const useFetchData = (url) => {
 
     const [ data, setData ] = useState([]);
