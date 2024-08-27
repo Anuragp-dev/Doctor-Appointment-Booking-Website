da231ea3eab39297c36abdfd968fefc90e63eee5
ANURAG P <anuragp212@gmail.com>
2024-08-27 14:59:22 +0530

    Update review.js

--- backend/Routes/review.js
+++ backend/Routes/review.js
@@ -3,7 +3,7 @@ import { createReview, getAllReviews } from '../Controller/reviewController.js';
 import { authenticate, restrict } from '../auth/verifyToken.js';
 
 
-//nested routes
+//nested routes 
 const router = express.Router({ mergeParams: true });
 
 
