603b990560195b7f73e4efd18c1bdcfd2c2403b9
ANURAG P <anuragp212@gmail.com>
2024-08-29 13:11:02 +0530

    Update doctors.js

--- frontend/src/assets/data/doctors.js
+++ frontend/src/assets/data/doctors.js
@@ -2,6 +2,8 @@ import doctorImg01 from "../images/doctor-img01.png";
 import doctorImg02 from "../images/doctor-img02.png";
 import doctorImg03 from "../images/doctor-img03.png";
 
+//sample data 
+
 export const doctors = [
   {
     id: "01",
