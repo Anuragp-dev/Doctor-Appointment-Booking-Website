b24f9bb9f027eb1cf12e442a1dbd3b2f8e6a3827
ANURAG P <anuragp212@gmail.com>
2024-09-07 11:51:28 +0530

    Update faqs.js

--- frontend/src/assets/data/faqs.js
+++ frontend/src/assets/data/faqs.js
@@ -1,5 +1,5 @@
 export const faqs = [
-  { 
+  {  
     question: "What is your medical care?",
     content:
       "One Medical was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
