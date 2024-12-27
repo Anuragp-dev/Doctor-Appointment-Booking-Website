bda4440743aabd7a40168424656f29ec2b46c3ea
ANURAG P <anuragp212@gmail.com>
2024-12-27 14:20:58 +0530

    Update doctors.js

--- frontend/src/assets/data/doctors.js
+++ frontend/src/assets/data/doctors.js
@@ -2,7 +2,7 @@ import doctorImg01 from "../images/doctor-img01.png";
 import doctorImg02 from "../images/doctor-img02.png";
 import doctorImg03 from "../images/doctor-img03.png";
 
-//sample data   
+//sample data  
 
 export const doctors = [
   {
